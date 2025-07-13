1ce8247ef7079f224c132a99d62cdeeb42cb226e
arvchahal <arnavchahal14@gmail.com>
2025-07-12 20:11:42 -0600

    more files

---
+++ src/dit_components/dit.py
@@ -0,0 +1,4 @@
+class DIT:
+    """
+    The actual distributed inference table that holds all of the locations for the models and what not
+    """
---
+++ src/routers/router.py
@@ -0,0 +1,4 @@
+class Router:
+    """
+    Base router class
+    """
