7f9b77f4f5654a9ba2570e6c12d2bfda1500e550
arvchahal <arnavchahal14@gmail.com>
2025-07-14 23:26:46 -0600

    working skeelton

--- src/dit_components/dit_router.py
+++ src/dit_components/dit_router.py
@@ -7,9 +7,9 @@ class DITRouter:
     def __init__(self, expert_table: dict[str, DitExpert],router:Router):
         self.expert_table = expert_table
         self.router:Router = router
-        #  zero-shot model for quick classification
-        self.zs = pipeline("zero-shot-classification",
-                           model="facebook/bart-large-mnli")
+        # #  zero-shot model for quick classification
+        # self.zs = pipeline("zero-shot-classification",
+        #                    model="facebook/bart-large-mnli")
 
     def route(self, query: str) -> str:
         return self.router.route(query)
