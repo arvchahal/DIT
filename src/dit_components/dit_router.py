8a436bde0eca00260e568acb386a9a3511a6c06e
arvchahal <arnavchahal14@gmail.com>
2025-07-14 23:11:49 -0600

    minor changes

--- src/dit_components/dit_router.py
+++ src/dit_components/dit_router.py
@@ -4,13 +4,13 @@ from collections import defaultdict
 from dit_components import DitModel
 
 class DITRouter:
-    def __init__(self, expert_table: dict[str, DitModel]):
+    def __init__(self, expert_table: dict[str, DitModel],router:Router):
         self.expert_table = expert_table
+        self.router:Router = router
         #  zero-shot model for quick classification
         self.zs = pipeline("zero-shot-classification",
                            model="facebook/bart-large-mnli")
 
     def route(self, query: str) -> str:
-        candidates = list(self.expert_table.keys())
-        res = self.zs(query, candidate_labels=candidates)
-        return res["labels"][0]
+        return router.route(query)
+    
